352b512b08559fcbe8fc200d18906bf38d04ad0c
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-10-09 15:11:52 +0200

    feat: set up supabase

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+export function initBrowserClient() {
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  ) {
+    throw new Error("failed to initialize client");
+  }
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+}
