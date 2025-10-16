7193a5321caae0f5240c3bdecb573af4567dbae8
Bobber <bobberen@protonmail.com>
2025-10-16 19:56:32 +0200

    added contact (mail) and maps link to adress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,17 @@ export default function RootLayout({
       <footer className="p-5 border-2 border-black rounded-lg mx-4 my-4">
           <div className="flex gap-10 mb-5 items-end">
               <div className="p-8 border-2 border-black rounded-lg">
-                  <p>Istedgade 101, 1 sal.<br/>Copenhagen<br/>1650, Vesterbro</p>
-                  <p>?contact info?</p>
+                  <a
+                      href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x46525375fac17d81:0x897dc1f7339cfb8f?sa=X&ved=1t:8290&ictx=111"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:underline"
+                  >
+                      <p>Istedgade 101, 1 sal.<br/>Copenhagen<br/>1650, Vesterbro</p>
+                  </a>
+                  <a href="mailto:Istedgade101@gmail.com" className="hover:underline">
+                      <p>Istedgade101@gmail.com</p>
+                  </a>
               </div>
               <div className="p-5 border-2 border-black rounded-lg flex-1 flex items-center justify-center gap-4">
                   <a href="https://instagram.com/bebsisbadekar/" target="_blank" rel="noopener noreferrer">
