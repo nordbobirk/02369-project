7eba124e31d85f0670711cf2cd106304cf758c9c
MoellerGutten <s245971@student.dtu.dk>
2025-10-21 16:57:07 +0200

    Removed test folder and added empty files for dashboard and settings

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world, I am a dashboard
+    </div>
+  );
+}
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world, I am the settings page
+    </div>
+  );
+}
