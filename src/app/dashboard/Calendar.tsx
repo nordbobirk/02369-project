7531281c0af5f65a85761ca95de5e0e086597c8e
MoellerGutten <s245971@student.dtu.dk>
2025-10-23 15:44:03 +0200

    Rest of page gets data from database

--- src/app/dashboard/Calendar.tsx
+++ src/app/dashboard/Calendar.tsx
@@ -1,39 +1,35 @@
 "use client"
 
 import * as React from "react"
-import { formatDateRange } from "little-date"
-import { PlusIcon } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
-import { initServerClient } from "@/lib/supabase/server"
+import { getBookingsAtDate } from "./getBookings"
 
-// TODO get events from database
-const events = [
-  {
-    title: "Team Sync Meeting",
-    from: "2025-06-12T09:00:00",
-    to: "2025-06-12T10:00:00",
-  },
-  {
-    title: "Design Review",
-    from: "2025-06-12T11:30:00",
-    to: "2025-06-12T12:30:00",
-  },
-  {
-    title: "Client Presentation",
-    from: "2025-06-12T14:00:00",
-    to: "2025-06-12T15:00:00",
-  },
-]
+type Booking = {
+    id: string,
+    name: string,
+    date_and_time: string,
+}
 
 export default function Calendar31() {
-  const [date, setDate] = React.useState<Date | undefined>()
+  const [date, setDate] = React.useState<Date>(new Date())
+  const [bookings, setBookings] = React.useState<Booking[]>([])
+  const [loading, setLoading] = React.useState<boolean>(true)
 
   React.useEffect(() => {
     setDate(date)
   }, [])
+
+  React.useEffect(() => {
+    setLoading(true)
+    getBookingsAtDate(date).then((response) => {
+      setLoading(false)
+      setBookings(response)
+    })
+  }, [date])
+
+
 
   return (
     <Card className="w-fit py-4">
@@ -49,7 +45,7 @@ export default function Calendar31() {
       <CardFooter className="flex flex-col items-start gap-3 border-l px-4 !pt-4">
         <div className="flex w-full items-center justify-between px-1">
           <div className="text-sm font-medium">
-            {date?.toLocaleDateString("en-US", {
+            {date.toLocaleDateString("en-US", {
               day: "numeric",
               month: "long",
               year: "numeric",
@@ -58,17 +54,18 @@ export default function Calendar31() {
 
         </div>
         <div className="flex w-full flex-col gap-2">
-          {events?.map((event) => (
+          {loading ? <p>Loading</p> : bookings.length > 0 ? bookings.map((booking) => (
             <div
-              key={event.title}
+              key={booking.id}
               className="bg-muted after:bg-primary/70 relative rounded-md p-2 pl-6 text-sm after:absolute after:inset-y-2 after:left-2 after:w-1 after:rounded-full"
             >
-              <div className="font-medium">{event.title}</div>
+              <div className="font-medium">{booking.name}</div>
               <div className="text-muted-foreground text-xs">
-                {formatDateRange(new Date(event.from), new Date(event.to))}
+                {"Important info"}
               </div>
             </div>
-          ))}
+          )) : <p>No bookings today</p>
+          }
         </div>
       </CardFooter>
     </Card>
