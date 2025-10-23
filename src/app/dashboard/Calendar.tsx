2e61f2b4d09007f6840066f1263603ff9822fe61
MoellerGutten <s245971@student.dtu.dk>
2025-10-23 16:14:54 +0200

    added spinner

--- src/app/dashboard/Calendar.tsx
+++ src/app/dashboard/Calendar.tsx
@@ -54,7 +54,8 @@ export default function Calendar31() {
 
         </div>
         <div className="flex w-full flex-col gap-2">
-          {loading ? <p>Loading</p> : bookings.length > 0 ? bookings.map((booking) => (
+          {/* DO NOT TOUCH MY SPINNER ! */}
+          {loading ? <p className="animate-spin flex justify-center text-4xl text-bold">c</p> : bookings.length > 0 ? bookings.map((booking) => (
             <div
               key={booking.id}
               className="bg-muted after:bg-primary/70 relative rounded-md p-2 pl-6 text-sm after:absolute after:inset-y-2 after:left-2 after:w-1 after:rounded-full"
