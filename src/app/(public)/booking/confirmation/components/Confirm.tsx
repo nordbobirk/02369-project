e72b1726b36194bb43a9dc310e43e3feb09e3f32
s245299 <s245299@dtu.dk>
2025-11-01 12:35:17 +0100

    Small changes

--- src/app/(public)/booking/confirmation/components/Confirm.tsx
+++ src/app/(public)/booking/confirmation/components/Confirm.tsx
@@ -1,19 +1,18 @@
-"use client"; // only needed if you'll use hooks like useState
-
-import { useState } from "react";
-import {CheckCircle} from "lucide-react";
-// import { BookingFormData } from "@/app/(public)/booking/_components/Form"; // uncomment when you actually use it
+"use client";
+import { CheckCircle } from "lucide-react";
 
 export default function Confirm() {
     return (
-        <>
-            <h1 className="mb-6 flex items-center justify-center gap-3 text-2xl font-bold text-white">
-                <CheckCircle className="size-40 text-green-500" />
-            </h1>
+        <section className="-mx-[calc(50vw-50%)] w-screen min-h-svh border-2 border-black p-8">
+            <div className="pointer-events-none fixed inset-0 z-50 border-2 border-black" />
+                <h1 className="mb-6 flex items-center justify-center gap-3 text-2xl font-bold text-white">
+                    <CheckCircle className="h-40 w-40 text-green-500" />
+                </h1>
 
-            <h2 className="mb-6 flex items-center justify-center gap-3 text-2xl font-bold text-black">
-                <span>Din booking er hermed indsendt </span>
+            <h2 className="mb-6 flex items- justify-center gap-3 text-2xl font-bold text-black">
+                <span>Din booking er hermed indsendt</span>
             </h2>
-        </>
+        </section>
     );
 }
+
