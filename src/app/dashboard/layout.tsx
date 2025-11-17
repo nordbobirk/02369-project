991e81a3a3e492eaa716d101f285725c281407c7
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-11-17 13:34:30 +0100

    fix: imports

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { BookImageIcon, CalendarIcon, HomeIcon, SettingsIcon } from "lucide-react";
+import { CalendarIcon, CircleQuestionMarkIcon, HomeIcon, SettingsIcon } from "lucide-react";
 import Link from "next/link";
 import { DropdownMenuDemo } from "./DropdownMenu";
 
