cad3846a3bf5b720381aeef567e1d97c0ab36edd
Raaschou <mraaschou@gmail.com>
2025-11-25 22:39:07 +0100

    Add dropdown-menu test

---
+++ src/components/ui/_test/dropdown-menu.test.tsx
@@ -0,0 +1,43 @@
+import {screen, render} from "@testing-library/react"
+import * as dpdw from "../dropdown-menu"
+
+describe("Dropdown menu", () => {
+  it("renders dropdown menu", () => {
+    render(
+      <dpdw.DropdownMenu>
+        <dpdw.DropdownMenuPortal></dpdw.DropdownMenuPortal>
+        <dpdw.DropdownMenuTrigger></dpdw.DropdownMenuTrigger>
+        <dpdw.DropdownMenuContent>
+          <dpdw.DropdownMenuLabel>dropdown label</dpdw.DropdownMenuLabel>
+          <dpdw.DropdownMenuSeparator/>
+          <dpdw.DropdownMenuGroup>
+            <dpdw.DropdownMenuItem>
+              Menu item 1
+              <dpdw.DropdownMenuShortcut></dpdw.DropdownMenuShortcut>
+            </dpdw.DropdownMenuItem>
+          </dpdw.DropdownMenuGroup>
+          <dpdw.DropdownMenuSeparator />
+          <dpdw.DropdownMenuCheckboxItem checked>
+            Checkbox item
+          </dpdw.DropdownMenuCheckboxItem>
+          <dpdw.DropdownMenuSeparator />
+          <dpdw.DropdownMenuRadioGroup value="test">
+            <dpdw.DropdownMenuRadioItem value="test">
+              test
+            </dpdw.DropdownMenuRadioItem>
+          </dpdw.DropdownMenuRadioGroup>
+          <dpdw.DropdownMenuSeparator />
+          <dpdw.DropdownMenuSub>
+            <dpdw.DropdownMenuSubTrigger></dpdw.DropdownMenuSubTrigger>
+            <dpdw.DropdownMenuSubContent>
+              <dpdw.DropdownMenuItem></dpdw.DropdownMenuItem>
+            </dpdw.DropdownMenuSubContent>
+          </dpdw.DropdownMenuSub>
+        </dpdw.DropdownMenuContent>
+      </dpdw.DropdownMenu>
+    )
+    
+    expect(screen.getByText("Menu item 1")).toBeInTheDocument();
+    expect(screen.getByText("test")).toBeInTheDocument();
+  })
+})
