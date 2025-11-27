b493941570901a2caafbf2829e5317e722f7de7f
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-11-27 12:29:48 +0100

    Update _BookingRequestReceived.test.tsx

--- src/components/email/customer/_test/_BookingRequestReceived.test.tsx
+++ src/components/email/customer/_test/_BookingRequestReceived.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import BookingRequestReceived from "./BookingRequestReceived";
+import BookingRequestReceived from "../BookingRequestReceived";
 
 describe("BookingRequestReceived", () => {
   const mockManageBookingLink = "https://example.com/manage/booking123";
