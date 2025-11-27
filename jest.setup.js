904dce9bd8b1d2a40584639fb21cc34f318b0971
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-11-27 14:08:02 +0100

    globally mock customer email util

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,14 @@
 import '@testing-library/jest-dom'
 
 import { sendEmail } from "./src/lib/email/send";
+import { getCustomerEmail } from "./src/lib/email/validate"
 
 jest.mock("./src/lib/email/send", () => ({
     sendEmail: jest.fn(),
+}))
+
+jest.mock("./src/lib/email/validate", () => ({
+    getCustomerEmail: jest.fn().mockReturnValue("customer@example.com")
 }))
 
 beforeAll(() => {
