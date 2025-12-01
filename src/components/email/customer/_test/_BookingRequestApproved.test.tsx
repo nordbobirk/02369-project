1f30f4d8693b6505cbfeadc9d7dc4cb7d02ba333
Birk Nordbo <36029360+nordbobirk@users.noreply.github.com>
2025-12-01 21:50:36 +0100

    fix: ts error in test

--- src/components/email/customer/_test/_BookingRequestApproved.test.tsx
+++ src/components/email/customer/_test/_BookingRequestApproved.test.tsx
@@ -2,13 +2,12 @@ import { render, screen } from "@testing-library/react";
 import BookingRequestApproved from "../BookingRequestApproved";
 
 describe("BookingRequestApproved", () => {
-  const mockManageBookingLink = "https://example.com/manage/booking123";
-
   it("renders the email with correct title", () => {
-    render(<BookingRequestApproved manageBookingLink={mockManageBookingLink} />);
-    
-    const title = screen.getAllByText("Din bookinganmodning er blevet godkendt");
+    render(<BookingRequestApproved />);
+
+    const title = screen.getAllByText(
+      "Din bookinganmodning er blevet godkendt"
+    );
     expect(title[0]).toBeInTheDocument();
   });
-
 });
